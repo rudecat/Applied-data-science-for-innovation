13062831fa1faf1370b869c7905e6a7c5877e4ad
Kai-Ping Wang <rudecat2000@iinet.net.au>
2021-01-30 01:02:00 +1100

    Commit notebooks and reports

---
+++ assignment-2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predict how likely an existing customer will buy a new car',
+    author='Kai-Ping Wang',
+    license='',
+)
